4fcb3009e0d04214223d45e10bb4ee538f3be08a
AleksHolland84 <43878016+AleksHolland84@users.noreply.github.com>
2025-11-14 17:04:24 +0100

    Update basic-requests.py

--- basic-requests.py
+++ basic-requests.py
@@ -7,7 +7,7 @@ import requests #importerer requests modulet
 #---------------------------------------------------------#
 #                        VARIABLER                        #
 #---------------------------------------------------------#
-# URL'ens opbygning: Protocol (http://),  IP/DOMÆNENAVN (192.168.0.79), Port (:5000) og Sti (/process)
+# URL'ens opbygning: Protocol (http://),  IP/DOMÆNENAVN (e.g. 192.168.0.79), Port (:5000) og Sti (/process)
 url = "http://<IP-ADDRESS>:5000/process" # ændre IP-ADDRESSE 
 pin = "0001" # Vi sætter vores pinkode til "0001" # 
 payload = {"pincode": pin} # data der skal sendes der 
@@ -15,7 +15,7 @@ payload = {"pincode": pin} # data der skal sendes der
 
 # Sæt response-objektet til at være "response". 
 # Response-objektet sender et POST request til vres url med vores payload.
-response = requests.post(f"http://{url}", data = payload)
+response = requests.post(f"{url}", data = payload)
 
 # Hvis respons attributten (.text) indeholder "DENIED" skal terminale printe PIN + vores pinkode + DENIED
 if "DENIED"  in response.text:
